d20526275a46d7fffe426a00acb70b30698d572c
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 12:06:30 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan15-Logika/src/pbo1/pkg10118031/latihan15/logika/PBO110118031Latihan15Logika.java
@@ -0,0 +1,24 @@
+package pbo1.pkg10118031.latihan15.logika;
+
+/**
+ *
+ * @author Vendetta
+ * NAMA              : Rizki Aria Putra
+ * KELAS             : PBO1
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk 
+ *                     logika true false
+ */
+public class PBO110118031Latihan15Logika {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("a && b = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+
+    }
+
+}
